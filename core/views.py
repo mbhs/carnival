852fa400e76c081c048b42e8a82301657038e6cf
Mikhail Khrenov <mkhrenov34@gmail.com>
2017-11-21 21:25:47 -0500

    Shuffled URLs about, added new views

--- core/views.py
+++ core/views.py
@@ -4,3 +4,13 @@ from django.shortcuts import render
 def index(request):
     context = {}
     return render(request, 'core/index.html', context)
+
+
+def instruments(request):
+    context = {}
+    return render(request, 'core/instruments.html', context)
+
+
+def organizers(request):
+    context = {}
+    return render(request, 'core/organizers.html', context)
