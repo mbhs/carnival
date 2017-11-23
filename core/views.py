13af798bc95911bba2c186afde358384b512942d
Mark Jung <markojungo@gmail.com>
2017-11-22 21:01:21 -0500

    added about page

--- core/views.py
+++ core/views.py
@@ -2,5 +2,13 @@ from django.shortcuts import render
 
 
 def index(request):
-    context = {}
+    context = {
+        'navbar' : 'home'
+    }
     return render(request, 'core/index.html', context)
+
+def about(request):
+    context = {
+        'navbar' : 'about'
+    }
+    return render(request, 'core/about.html', context)
