5a0762e270e8f2161128d343151f62a706531f51
Mark Jung <markojungo@gmail.com>
2017-11-29 23:20:21 -0500

    design changes to index not final color scheme

--- static/core/js/scroll.js
+++ static/core/js/scroll.js
@@ -1,6 +1,6 @@
 $(function() {
   $("a[href*='#']").on('click', function(e) {
     e.preventDefault();
-    $('html, body').animate({ scrollTop: $($(this).attr('href')).offset().top}, 500, 'linear');
+    $('html, body').animate({ scrollTop: $($(this).attr('href')).offset().top - 50}, 500, 'linear');
   });
 });
