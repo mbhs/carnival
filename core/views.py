96a7ccb091eab77a8a4ca3c56d6db8bbcf1f6cf9
Mikhail Khrenov <mkhrenov34@gmail.com>
2017-12-18 21:11:45 -0500

    Fixed typos, added Chamber Orchestra performance

--- core/views.py
+++ core/views.py
@@ -1,4 +1,3 @@
-from django.http import FileResponse
 from django.shortcuts import render
 
 
@@ -21,3 +20,7 @@ def espanol(request):
         'navbar': 'espanol'
     }
     return render(request, 'core/espanol.html', context)
+
+
+def contact(request):
+    return
