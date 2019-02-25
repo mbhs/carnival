7d177a675dad8cc2d84e233d2e949421c58e9c4e
Jesse Silverberg <jessesilverberg11@gmail.com>
2019-02-25 13:56:33 -0500

    remove crispy

--- carnival/settings.py
+++ carnival/settings.py
@@ -43,10 +43,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'django.contrib.sitemaps',
     'django.contrib.sites',
-    'crispy_forms',
 ]
-
-CRISPY_TEMPLATE_PACK = 'bootstrap4'
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
