d7499965dfe4962cbfd19dcee3dcba2a6cd93696
Jesse Silverberg <jessesilverberg11@gmail.com>
2019-02-26 16:07:23 -0500

    remove logger

--- carnival/settings.py
+++ carnival/settings.py
@@ -111,25 +111,6 @@ AUTH_PASSWORD_VALIDATORS = [
     },
 ]
 
-LOGGING = {
-    'version': 1,
-    'disable_existing_loggers': False,
-    'handlers': {
-        'file': {
-            'level': 'DEBUG',
-            'class': 'logging.FileHandler',
-            'filename': os.path.join(BASE_DIR, 'debug.log'),
-        },
-    },
-    'loggers': {
-        'django': {
-            'handlers': ['file'],
-            'level': 'DEBUG',
-            'propagate': True,
-        },
-    },
-}
-
 # Internationalization
 # https://docs.djangoproject.com/en/1.11/topics/i18n/
 
