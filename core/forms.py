c2acb2bda24485781c347f42164b9b9843c2971c
Jesse Silverberg <jessesilverberg11@gmail.com>
2019-03-01 15:26:17 -0500

    add multiple file support

--- core/forms.py
+++ core/forms.py
@@ -5,3 +5,6 @@ class ImageForm(forms.ModelForm):
     class Meta:
         model = Image
         fields = ('picture', )
+        widgets = {
+            'picture': forms.ClearableFileInput(attrs={'multiple': True,'style': 'display: none;'})
+        }
