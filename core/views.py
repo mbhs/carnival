0450e9bdbed7a8b5956dd551818f7dbdb9cc8ee6
Jesse Silverberg <jessesilverberg11@gmail.com>
2019-03-04 17:18:47 -0500

    remove imageform dependency

--- core/views.py
+++ core/views.py
@@ -38,39 +38,37 @@ def contact(request):
 
 def dayof(request):
     if request.method == 'POST':
-        form = ImageForm(request.POST,request.FILES)
-        if form.is_valid():
-            #image = Image()
-            files = request.FILES.getlist('picture')
-
-
-            #image.picture = form.cleaned_data["picture"]
-            #image.save()
-            creds = None
-            SCOPES = ['https://www.googleapis.com/auth/drive']
-            if os.path.exists('token.pickle'):
-                with open('token.pickle', 'rb') as token:
-                    creds = pickle.load(token)
-            if not creds or not creds.valid:
-                if creds and creds.expired and creds.refresh_token:
-                    creds.refresh(Request())
-                else:
-                    flow = InstalledAppFlow.from_client_secrets_file('credentials.json', SCOPES)
-                    creds = flow.run_local_server()
-                with open('token.pickle', 'wb') as token:
-                    pickle.dump(creds, token)
-            service = build('drive', 'v3', credentials=creds)
-            folder_id = '1dOy-chl03stjflrlu4jl6nxyWhvAZNmR'
-            for f in files:
-                #print(f)
-                #image.pics.append(f)
-                save_path = os.path.join(settings.MEDIA_ROOT, str(f))
-                default_storage.save(str(f), f)
-                file_metadata = {'name': str(f),'parents': [folder_id]}
-                media = MediaFileUpload(save_path,mimetype='image/jpeg')
-                service.files().create(body=file_metadata,media_body=media,fields='id').execute()
-                default_storage.delete(str(f))
-            #image.delete()
+        #form = ImageForm(request.POST,request.FILES)
+        #if form.is_valid():
+        #image = Image()
+        files = request.FILES.getlist('picture')
+        #image.picture = form.cleaned_data["picture"]
+        #image.save()
+        creds = None
+        SCOPES = ['https://www.googleapis.com/auth/drive']
+        if os.path.exists('token.pickle'):
+            with open('token.pickle', 'rb') as token:
+                creds = pickle.load(token)
+        if not creds or not creds.valid:
+            if creds and creds.expired and creds.refresh_token:
+                creds.refresh(Request())
+            else:
+                flow = InstalledAppFlow.from_client_secrets_file('credentials.json', SCOPES)
+                creds = flow.run_local_server()
+            with open('token.pickle', 'wb') as token:
+                pickle.dump(creds, token)
+        service = build('drive', 'v3', credentials=creds)
+        folder_id = '1dOy-chl03stjflrlu4jl6nxyWhvAZNmR'
+        for f in files:
+            #print(f)
+            #image.pics.append(f)
+            save_path = os.path.join(settings.MEDIA_ROOT, str(f))
+            default_storage.save(str(f), f)
+            file_metadata = {'name': str(f),'parents': [folder_id]}
+            media = MediaFileUpload(save_path,mimetype='image/jpeg')
+            service.files().create(body=file_metadata,media_body=media,fields='id').execute()
+            default_storage.delete(str(f))
+        #image.delete()
 
     else:
         form = ImageForm()
